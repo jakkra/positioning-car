a49e39e59a3ce49a5d481bd229bef0c772794573
Jakob Krantz <mail@jakobkrantz.se>
2020-11-06 00:10:30 +0100

    Fix missmatching indentation.

--- main/main.c
+++ main/main.c
@@ -45,37 +45,37 @@ static uint16_t get_channel_value(uint16_t channel) {
 }
 
 static void driving_task(void *ignore) {
-	mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, STEER_SERVO_PIN);
-	mcpwm_gpio_init(MCPWM_UNIT_1, MCPWM0A, MOTOR_PIN);
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, STEER_SERVO_PIN);
+    mcpwm_gpio_init(MCPWM_UNIT_1, MCPWM0A, MOTOR_PIN);
 
-	mcpwm_config_t pwm_config;
+    mcpwm_config_t pwm_config;
     pwm_config.frequency = 50;
     pwm_config.cmpr_a = 0;
     pwm_config.cmpr_b = 0;
     pwm_config.counter_mode = MCPWM_UP_COUNTER;
     pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
     mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);
-	mcpwm_init(MCPWM_UNIT_1, MCPWM_TIMER_0, &pwm_config);
+    mcpwm_init(MCPWM_UNIT_1, MCPWM_TIMER_0, &pwm_config);
 
-	while (true) {
-		uint16_t rc_value = get_channel_value(RC_STEER_CHANNEL);
-		uint32_t duty_us = map(rc_value, 1000, 2000, MIN_STEER_VAL, MAX_STEER_VAL);
-		mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, duty_us);
+    while (true) {
+        uint16_t rc_value = get_channel_value(RC_STEER_CHANNEL);
+        uint32_t duty_us = map(rc_value, 1000, 2000, MIN_STEER_VAL, MAX_STEER_VAL);
+        mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, duty_us);
 
-		rc_value = get_channel_value(RC_MOTOR_CHANNEL);
-		mcpwm_set_duty_in_us(MCPWM_UNIT_1, MCPWM_TIMER_0, MCPWM_OPR_A, rc_value);
-		rc_value = get_channel_value(RC_MODE_SW2_CHANNEL);
-		if (rc_value < 1400) {
-			gpio_set_level(SWITCH_CONTROL_BUTTON_PIN, 0);
-		} else {
-			gpio_set_level(SWITCH_CONTROL_BUTTON_PIN, 1);
-		}
-		vTaskDelay(pdMS_TO_TICKS(10));
-	}
+        rc_value = get_channel_value(RC_MOTOR_CHANNEL);
+        mcpwm_set_duty_in_us(MCPWM_UNIT_1, MCPWM_TIMER_0, MCPWM_OPR_A, rc_value);
+        rc_value = get_channel_value(RC_MODE_SW2_CHANNEL);
+        if (rc_value < 1400) {
+            gpio_set_level(SWITCH_CONTROL_BUTTON_PIN, 0);
+        } else {
+            gpio_set_level(SWITCH_CONTROL_BUTTON_PIN, 1);
+        }
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
 }
 
 static void configure_switch_gpio(void) {
-	gpio_config_t io_conf;
+    gpio_config_t io_conf;
     io_conf.intr_type = GPIO_INTR_DISABLE;
     io_conf.mode = GPIO_MODE_OUTPUT;
     io_conf.pin_bit_mask = 1ULL << SWITCH_CONTROL_BUTTON_PIN;
@@ -209,19 +209,19 @@ void app_main() {
     }
     ESP_ERROR_CHECK(ret);
 
-	rc_receiver_rmt_init();
-	configure_switch_gpio();
-	xTaskCreate(&driving_task,"receiver_task", 2048, NULL, 5, NULL);
+    rc_receiver_rmt_init();
+    configure_switch_gpio();
+    xTaskCreate(&driving_task,"receiver_task", 2048, NULL, 5, NULL);
     
-	webserver_init(&handle_websocket_status);
+    webserver_init(&handle_websocket_status);
 #ifdef USE_STATION
     start_station();
 #else
-	start_ap();
+    start_ap();
 #endif
     webserver_start();
     initialise_mdns();
-	ESP_LOGW(TAG, "Started and running\n");
+    ESP_LOGW(TAG, "Started and running\n");
 }
 
 uint32_t map(uint32_t x, uint32_t in_min, uint32_t in_max, uint32_t out_min, uint32_t out_max) {
